5d79acc8ec941611cbdfdde6fd0f902ffd69dc98
hectottang <taotao.tang@gmail.com>
2025-07-20 11:23:20 +0800

    feat: add set default value field plugin

---
+++ packages/plugins/@nocobase/plugin-set-default-value/README.md
@@ -0,0 +1,3 @@
+# Set Default Value Button
+
+Add a button to set the current field value as the default value.
---
+++ packages/plugins/@nocobase/plugin-set-default-value/src/client/index.tsx
@@ -0,0 +1,92 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ */
+import { Field } from '@formily/core';
+import { observer, useField, useFieldSchema } from '@formily/react';
+import { Plugin, useDesignable, useToken } from '@nocobase/client';
+import { Button, Tooltip } from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import _ from 'lodash';
+
+const NAMESPACE = 'set-default-value';
+
+const SetDefaultValueButton: FC = observer(
+  () => {
+    const field = useField<Field>();
+    const fieldSchema = useFieldSchema();
+    const { dn } = useDesignable();
+    const { token } = useToken();
+    const { t } = useTranslation(NAMESPACE);
+
+    const handleClick = async () => {
+      fieldSchema.default = field.value;
+      await dn.emit('patch', {
+        schema: {
+          'x-uid': fieldSchema['x-uid'],
+          default: field.value,
+        },
+      });
+      dn.refresh();
+    };
+
+    return (
+      <Tooltip title={t('Set as default value')}>
+        <Button
+          type="text"
+          size="small"
+          icon={<SaveOutlined />}
+          onClick={handleClick}
+          style={{ marginLeft: token.marginXXS }}
+        />
+      </Tooltip>
+    );
+  },
+  {
+    displayName: 'SetDefaultValueButton',
+  },
+);
+
+class PluginSetDefaultValue extends Plugin {
+  async load() {
+    const setDefaultButton = <SetDefaultValueButton />;
+
+    this.app.addScopes({
+      SetDefaultValueButton: setDefaultButton,
+    });
+
+    this.app.schemaSettingsManager.addItem('fieldSettings:component:Input', 'enableSetDefault', {
+      type: 'switch',
+      useComponentProps() {
+        const { t } = useTranslation(NAMESPACE);
+        const fieldSchema = useFieldSchema();
+        const field = useField();
+        const { dn } = useDesignable();
+        return {
+          title: t('Display set default button'),
+          checked: !!fieldSchema['x-component-props']?.addonAfter,
+          onChange: async (checked) => {
+            if (checked) {
+              field.componentProps.addonAfter = setDefaultButton;
+              _.set(fieldSchema, 'x-component-props.addonAfter', '{{SetDefaultValueButton}}');
+            } else {
+              field.componentProps.addonAfter = null;
+              _.unset(fieldSchema, 'x-component-props.addonAfter');
+            }
+            await dn.emit('patch', {
+              schema: {
+                'x-uid': fieldSchema['x-uid'],
+                'x-component-props': {
+                  ...fieldSchema['x-component-props'],
+                },
+              },
+            });
+          },
+        };
+      },
+    });
+  }
+}
+
+export default PluginSetDefaultValue;
